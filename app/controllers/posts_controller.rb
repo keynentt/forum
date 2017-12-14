63441c5935db1e78d1853d67d816d12f49445196
Keynent B. Talip <keynentt@gmail.com>
2017-12-14 11:48:18 +0800

    Add up some styles, most of it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,17 @@
 class PostsController < ApplicationController
-before_action :find_post, only: [:show, :edit, :update, :destroy]
-before_action :authenticate_user!, except: [:index, :show]
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
-		@posts = Post.all.order("created_at DESC")	
+		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
-		
 	end
-
 
 	def new
 		@post = current_user.posts.build
 	end
-
 
 	def create
 		@post = current_user.posts.build(post_params)
@@ -22,19 +19,18 @@ before_action :authenticate_user!, except: [:index, :show]
 		if @post.save
 			redirect_to @post
 		else
-			render 'new' 
+			render 'new'
 		end
 	end
 
 	def edit
-		
 	end
 
 	def update
 		if @post.update(post_params)
 			redirect_to @post
 		else
-			render 'edit'	
+			render 'edit'
 		end
 	end
 
@@ -51,8 +47,5 @@ before_action :authenticate_user!, except: [:index, :show]
 
 	def post_params
 		params.require(:post).permit(:title, :content)
-		
 	end
-
-
 end
